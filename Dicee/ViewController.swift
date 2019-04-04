c9ea4ff02383e2390cb06cbda24c2b3f56c4acc2
Vahan Asryan <vahan.asryan@avetiq.de>
2019-04-04 20:01:03 +0400

    Generate random dice on Roll button touch

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -9,6 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+    
+    let diceArray = [
+        "dice1",
+        "dice2",
+        "dice3",
+        "dice4",
+        "dice5",
+        "dice6",
+    ]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -18,6 +30,11 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
     }
 }
 
