bb9306c3e81e7412c74e73c5b1a0af0b65b02c1b
Vahan Asryan <vahan.asryan@avetiq.de>
2019-04-04 16:36:41 +0400

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Vahan Asryan on 4/4/19.
+//  Copyright © 2019 Vahan Asryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
